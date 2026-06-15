7cf7fe4f077e5272704fe16cdfecdf32c9c5c8c8
dt <106457860+tomidt@users.noreply.github.com>
2026-06-15 13:59:26 -1000

    update gauge location api

---
+++ src/api/update_gauge_location.js
@@ -0,0 +1,35 @@
+// const { getActiveLocations } = require("./get_active_locations.js");
+
+const { getActiveLocationsDB } = require("../database/queries.js");
+
+let ACTIVE_LOCATIONS = null;
+let ACTIVE_LOCATIONS_FLAT = null;
+let ACTIVE_LOCATIONS_DATE = null;
+
+module.exports = {
+    updateGaugeLocation
+};
+
+async function updateGaugeLocation(flat) {
+    const today = new Date().toDateString();
+    if (ACTIVE_LOCATIONS && ACTIVE_LOCATIONS_DATE == today) {
+        return flat ? ACTIVE_LOCATIONS_FLAT : ACTIVE_LOCATIONS;
+    }
+
+    const activeLocations = await getActiveLocationsDB(['gauge_type']);
+    ACTIVE_LOCATIONS = {};
+    ACTIVE_LOCATIONS_FLAT = {};
+
+    for (const [gaugeType, locationsArray] of Object.entries(activeLocations)) {
+        ACTIVE_LOCATIONS[gaugeType] = locationsArray
+            .map(loc => loc.gauge_id)
+            .join(',');
+        
+        locationsArray.forEach(loc => {
+            ACTIVE_LOCATIONS_FLAT[loc.gauge_id] = gaugeType;
+        });
+    }
+
+    ACTIVE_LOCATIONS_DATE = today;
+    return flat ? ACTIVE_LOCATIONS_FLAT : ACTIVE_LOCATIONS;
+}
